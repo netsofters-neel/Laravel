2d0b77314063c7f272d67cc8ef81c4ed7498fd6f
Neel Sheth <108276436+neelsheth@users.noreply.github.com>
2024-03-17 16:01:59 +0530

    Update set_up.php

--- set_up.php
+++ set_up.php
@@ -1,8 +1,25 @@
 //create project
 composer create-project laravel/laravel crud
 
+
 //live
 php artisan serve
 
-//migrate/ create database tables
+
+//migrate database tables
+php artisan make:migration create_NameOfTable_table
 php artisan migrate
+
+
+//generate model
+php artisan make:model Name
+
+
+//generate controller
+php artisan make:controller NameController
+
+
+//make route
+Route::get('/products',[NameController::class,'index']);   //index will replaced by function name
+or
+Route::get('/products',[NameController::class,'index'])->name('name.index'); // it will give name , so no need to edit all Href in case of url edit
